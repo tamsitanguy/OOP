fb8956989c96ad4d58dd2957416b5a9a0b923e9f
Tamsi <tamsi_tanguyb@hotmail.com>
2019-08-28 12:43:52 -0400

    progress Abdigani and Tanguy

--- src/main/java/myAssignment/Dog.java
+++ src/main/java/myAssignment/Dog.java
@@ -25,7 +25,7 @@ public class Dog extends Pet implements Serializable {
 	//running speed must be at least 10mph
 	public static void showSpeed() {
 		if(speed<10) {
-			System.out.println("This dog is not running"+speed);
+			System.out.println("This dog is not running "+speed);
 		}
 		else {
 			System.out.println("This dog is running at :"+speed +" mph");
--- src/main/java/myAssignment/PetDriver.java
+++ src/main/java/myAssignment/PetDriver.java
@@ -5,7 +5,13 @@ public class PetDriver {
 
 	public static void main(String[] args) {
 	
-		System.out.println(new Cat());	
+		System.out.println(new Cat());
+		
+		Dog.showSpeed();
+		Dog d = new Dog();
+		d.run();
+		Pet p = new Dog();
+		//System.out.println(new .showSpeed());
 
 	}
 
